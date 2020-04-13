fa5fda1f6265cb6e8e57fb9e75965852ad2906fe
Matt Worthington <worthington.matthew@gmail.com>
2020-04-13 11:06:24 -0500

    Add COVID Data Tracker, Add Data Directories, Update CSS, and Fix Data Feeds for The Published Texas 2036 Version

--- app.R
+++ app.R
@@ -30,18 +30,19 @@ map_attr <- "<a href='https://www.mapbox.com/map-feedback/'>© MAPBOX</a> | <a h
 # **Coronavirus Data -----------------------------------------------------------
 
 
-jhu_cases_state <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/archived_data/data/cases_state.csv") %>% 
+jhu_cases_state <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_state.csv") %>% 
   janitor::clean_names() %>% 
   filter(province_state=="Texas") %>% 
   mutate(fips=as.character(fips))
 
-jhu_cases_county <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/archived_data/data/cases.csv") %>% 
+jhu_cases_county <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases.csv") %>% 
   janitor::clean_names() %>% 
   rename(county=admin2,
          state=province_state,
          cases=confirmed) %>% 
   separate(last_update, into=c("date","time"), sep=" ") %>% 
-  mutate(fips=as.character(fips))
+  mutate(fips=as.character(fips),
+         mort_rate=round(deaths/cases))
   
 jhu_tx_cases_today <- jhu_cases_county %>% 
   mutate(date = ymd(date)) %>%
@@ -113,7 +114,7 @@ tx_series <-fredr(
 
 # HEADER CODE-----------------------------------------------------------
 
-header <- dashboardHeader(
+header <- dashboardHeader(disable = FALSE,
   title = tags$a(href='http://www.texas2036.org',
                  HTML('<svg viewBox="0 0 227.4 83.5" style="height:4.5vh;padding-bottom:1vh;margin-top:9px"><path fill="currentColor" d="M192.5 66.2c2.2 0 3.9.6 3.9 2.6v4.1c0 2-1.7 3.6-3.9 3.6-2.1 0-3.8-1.6-3.8-3.6v-5.1h-7.8v5.1c0 5.9 5.2 10.6 11.6 10.6 6.4 0 11.5-4.6 11.7-10.4.6 5.4 5.6 10.4 11.6 10.4 6.4 0 11.6-4.8 11.6-10.6v-7.4c0-5.8-5.2-10.6-11.6-10.6-1.4 0-2.7.2-3.9.6v-4.1c0-1.9 1.8-3.5 3.9-3.5 2.1 0 3.8 1.6 3.8 3.5v2.2h7.8v-2.2c0-4-2.5-7.5-6.1-9.3 3.6-1.8 6.1-5.3 6.1-9.3V10.5c0-5.8-5.2-10.5-11.6-10.5-6.1 0-11.1 4.3-11.6 9.8-.4-5.5-5.5-9.8-11.7-9.8-6.4 0-11.6 4.7-11.6 10.6v2.6h7.8v-2.6c0-1.9 1.7-3.5 3.8-3.5 2.2 0 3.9 1.6 3.9 3.5v.8l-.1.1-13 15.6c-2.3 2.8-2.4 3-2.4 5.9v10.5h4.1c-2.5 1.9-4.1 4.8-4.1 8v2.2h7.8v-2.2c0-1.9 1.7-3.5 3.8-3.5 2.2 0 3.9 1.6 3.9 3.5v4.1c0 2-1.7 3.6-3.9 3.6h-2.4v7.1h2.4zm19.4-55.6c0-1.9 1.7-3.5 3.8-3.5 2.1 0 3.8 1.6 3.8 3.5v20.7c0 2-1.7 3.6-3.8 3.6-2.1 0-3.8-1.6-3.8-3.6V10.6zm-7.8 57c-.3-1.9-1.3-3.3-2.9-5 1.6-1.6 2.6-3.7 2.9-5.9v10.9zm-15.4-32.8v-2.6l13.1-15.8c1.6-1.9 2.2-2.6 2.3-3.8v20.3c0 .5 0 .9.1 1.3l2.1 6.4h6.8l-5.5 4 2.1 6.5-5.5-4-5.5 4 2.1-6.5-5.5-4h6.8l1.9-5.9h-15.3zm30.9 38.1c0 2-1.7 3.6-3.8 3.6-2.2 0-3.9-1.6-3.9-3.6v-7.4c0-1.9 1.8-3.5 3.9-3.5 2.1 0 3.8 1.6 3.8 3.5v7.4zM8.4 82.7V8H0V0h24.8v8h-8.4v74.8h-8zm45.4 0H33V0h20.8v8H41v29.5h12.8v8H41v29.4h12.8v7.8zm70.2 0V45.3h-12.8v37.4h-8V14.4c0-8 6.5-14.4 14.4-14.4 7.8 0 14.3 6.5 14.3 14.4v68.3H124zm0-68.3c0-3.6-2.9-6.5-6.3-6.5-3.6 0-6.5 2.9-6.5 6.5v22.9H124V14.4zm37.6 6.1v-6.2c0-3.5-2.9-6.3-6.3-6.3-3.5 0-6.3 2.9-6.3 6.3v6.3c0 1.5 0 1.5.4 2.1l17.9 31.6c2.4 4.2 2.4 4.2 2.4 7.8v6.2c0 8-6.5 14.3-14.3 14.3S141 76.4 141 68.4v-6.2h8v6.2c0 3.6 2.9 6.5 6.3 6.5 3.5 0 6.3-2.9 6.3-6.5v-6.2c0-1.4 0-1.4-.4-2l-17.9-31.6c-2.4-4.2-2.4-4.2-2.4-8v-6.3C141 6.5 147.5 0 155.3 0s14.3 6.5 14.3 14.3v6.2h-8zM95.9 0h-8.2l-9.2 28.7L69.3 0h-8.2l13.3 41.6L61.1 83h8.3l9.1-28.5L87.6 83h8.3L82.6 41.6z"></path><svg>'),
                  tags$title('Texas COVID-19 Resource Kit'))
@@ -125,38 +126,21 @@ sidebar <- dashboardSidebar(disable = TRUE,
   collapsed = TRUE,
   sidebarMenu(
     id = "tabs",
-    menuItem("Case Impact", 
+    menuItem("Case Impact",
              tabName = "dashboard", icon = icon("dashboard")),
-    conditionalPanel(
-      condition = "input.tabs == 'dashboard'",
-      sliderInput("year", 
-                "Select a Year:",
-                min = as.Date("2013-01-01"),
-                max = as.Date("2016-01-01"),
-                value= as.Date("2013-01-01"),
-                timeFormat= "%Y",
-                step = 365,
-                animate = animationOptions(interval = 1000)),
-      selectInput(
-        inputId = "indicator",
-        label = "Select an Indicator",
-        choices = c("Male Population" = "Pct_Male",
-                    "Female Population" = "Pct_Female",
-                    "Married/Couple Population" = "Pct_MarriedCouple",
-                    "Single Parent Population" = "Pct_SingleParent",
-                    "Asian Population" = "Pct_Asian",
-                    "Black Population" = "Pct_Black",
-                    "Hispanic Population" = "Pct_Hispanic",
-                    "White Population" = "Pct_White",
-                    "Family Poverty Rate" = "Rate_FamPov",
-                    "Incomes to Poverty Ratio: 100%" = "Rate_Poverty100",
-                    "Incomes to Poverty Ratio: 200%" = "Rate_Poverty200",
-                    "Educational Attainment: Less Than HS" = "Rate_LessThanHS",
-                    # "Educational Attainment: Dropout" = "Rate_Dropout",
-                    "Coverage Rate: No Health Insurance" = "Rate_NoHealth"),
-        selected = "Pct_SingleParent"),
-      downloadButton('downloadCSV', label = 'Download Data', style="display: block; margin: 0 auto; width: 200px;color: black;")
-      ),
+    menuSubItem(
+      tabName = NULL,
+      text = shiny::tags$html("<a href='#anchorid0'>Go to anchor0</a>")
+    ),    # conditionalPanel(
+    #   condition = "input.tabs == 'dashboard'",
+    #   sliderInput("year", 
+    #             "Select a Year:",
+    #             min = as.Date("2013-01-01"),
+    #             max = as.Date("2016-01-01"),
+    #             value= as.Date("2013-01-01"),
+    #             timeFormat= "%Y",
+    #             step = 365,
+    #             animate = animationOptions(interval = 1000)),
     menuItem("About The Dashboard", icon = icon("file-text"), tabName = "about")
   )
 )
@@ -175,7 +159,7 @@ body <- dashboardBody(
   tabItems(
     tabItem(tabName = "dashboard",
             # hr(),
-            h2(style="font-weight:800;", "STATEWIDE COVID-19 PROFILE"),
+            h2(style="font-weight:800;", "STATEWIDE COVID-19 PROFILE", span="id='anchorid0'"),
             fluidRow(
               valueBoxOutput("tx_cases", width=3),
               # valueBoxOutput("tx_tests", width=2),
@@ -185,7 +169,7 @@ body <- dashboardBody(
               valueBoxOutput("test_rate", width=3)
               ),
             hr(),
-            h2(style="font-weight:800;","COUNTY COVID-19 PROFILE"),
+            h2(style="font-weight:800;","COUNTY COVID-19 PROFILE", span="id='anchorid1'"),
             fluidRow(
               column(width = 6,
                      h4("Data As of:",textOutput("currentTime", inline=TRUE)),
@@ -206,47 +190,47 @@ body <- dashboardBody(
                        h2(style="font-weight:800;text-align:center;", textOutput("county_active_text")),p(style="text-align:center","Active Cases")),
     
                 ),
-              highchartOutput("cnty_curves_hchart", height = 300),
               highchartOutput("cnty_compare_hchart", height = 100),
+              highchartOutput("cnty_curves_hchart", height = 300),
               box(solidHeader = TRUE, 
                   width=12, 
                   height = 600,
                   background = "navy",
                   collapsible = FALSE))
               )
-            ),
-    tabItem(tabName = "about",
-            fluidRow(
-              box(
-                title = "Calls & Calls Per 100 Households", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-                ),
-              box(
-                title = "Median Household Values & The Gini Index", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-                )
-              ),
-            fluidRow(
-              box(
-                title = "The Map Indicators & Indicator Percentile", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-              ),
-              box(
-                title = "Histogram", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-              )
-            ),
-            fluidRow(
-              box(
-                title = "Histogram", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-              ),
-              box(
-                title = "Histogram", status = "primary", solidHeader = TRUE,
-                collapsible = TRUE
-              )
             )
-            )
+    # tabItem(tabName = "about",
+    #         fluidRow(
+    #           box(
+    #             title = "Calls & Calls Per 100 Households", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #             ),
+    #           box(
+    #             title = "Median Household Values & The Gini Index", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #             )
+    #           ),
+    #         fluidRow(
+    #           box(
+    #             title = "The Map Indicators & Indicator Percentile", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #           ),
+    #           box(
+    #             title = "Histogram", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #           )
+    #         ),
+    #         fluidRow(
+    #           box(
+    #             title = "Histogram", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #           ),
+    #           box(
+    #             title = "Histogram", status = "primary", solidHeader = TRUE,
+    #             collapsible = TRUE
+    #           )
+    #         )
+    #         )
     )
 )
 
@@ -364,7 +348,7 @@ server <- function (input, output, session) {
     )
   })
   
-  # {Teting Rate} -----------------------------------------------
+  # {Testing Rate} -----------------------------------------------
   
   output$test_rate <- renderValueBox({
     tot_pos <- jhu_cases_state %>% 
@@ -451,8 +435,12 @@ output$map <- renderLeaflet({
 
     output$county_cases_text <- renderText({
       
+      # Make sure requirements are met
+      req(input$countyname)
+      
       tx_county_cases %>%
         filter(county==input$countyname) %>% 
+        mutate_at(vars(cases), scales::comma) %>% 
         distinct(cases) %>% 
         as.character()
     })
@@ -460,6 +448,9 @@ output$map <- renderLeaflet({
 # County Active Cases --------------------------------------------------
     
     output$county_active_text <- renderText({
+      
+      # Make sure requirements are met
+      req(input$countyname)
       
       tx_county_cases %>%
         filter(county==input$countyname) %>% 
@@ -471,6 +462,9 @@ output$map <- renderLeaflet({
   
     output$county_incident_text <- renderText({
       
+      # Make sure requirements are met
+      req(input$countyname)
+      
       tx_county_cases %>%
         filter(county==input$countyname) %>% 
         distinct(incident_rate) %>% 
@@ -481,6 +475,9 @@ output$map <- renderLeaflet({
 # County Confirmed Deaths -------------------------------------------------
 
     output$county_deaths_text <- renderText({
+      
+      # Make sure requirements are met
+      req(input$countyname)
       
       tx_county_cases %>%
         filter(county==input$countyname) %>% 
@@ -504,6 +501,9 @@ output$map <- renderLeaflet({
 
     output$cnty_compare_hchart <- renderHighchart({
       
+      # Make sure requirements are met
+      req(input$countyname)
+      
       nyt_county_cases_chart <- nyt_county_cases %>% 
         filter(county==input$countyname) %>% 
         mutate(date = ymd(date)) 
@@ -518,19 +518,31 @@ output$map <- renderLeaflet({
         filter(county==input$countyname)
       
       nyt_county_cases_today %>% 
-        hchart(type="scatter", hcaes(x = cases, y = 0, group=date, name = county), animation=FALSE,
-             color = "#fff") %>%
-        hc_add_series(data=nyt_highlight_cnty, type="scatter",  hcaes(x = cases, y = 0, name = county), size=2, color = "#5da5da", animation = FALSE) %>%
-        # hc_xAxis(labels = list(format = "{value}")) %>%
-        hc_tooltip(table = TRUE, sort = TRUE,
-                   pointFormat = "<b> {series.name}</b>
-                                  <br>Confirmed Cases: <b> {point.x:,.0f}</b>") %>%
+        hchart(type="scatter", hcaes(x = cases, y = 0, name = county), animation=FALSE,
+             color = "#fff",showInLegend=FALSE) %>%
+        hc_add_series(data=nyt_highlight_cnty, type="scatter",  hcaes(x = cases, y = 0,name = county), 
+                      color = "#FFD100", marker = list(radius=9, enabled=TRUE), animation = FALSE,
+                      showInLegend=FALSE, dataLabels = list(enabled = TRUE,
+                                                            padding=10,
+                                                            format = '{point.name} County')) %>%
+        hc_xAxis(labels=list(format = "{value:,.0f} Cases")) %>%
+        hc_yAxis(title=FALSE,
+          showFirstLabel = FALSE,
+          showLastLabel = FALSE
+        ) %>% 
+        hc_tooltip(table = TRUE, sort = TRUE, borderWidth = 0,
+                   pointFormat = "<b>{this.name}</b><br>
+                                  Confirmed Cases: <b> {point.x:,.0f}</b>") %>%
         hc_add_theme(
           hc_theme_merge(
             hc_theme_smpl(),
             hc_theme(chart = list(backgroundColor = "#3A4A9F",
                                   style = list(fontFamily = "Montserrat")),
-                     yAxis = list(labels =list(style = list(fontFamily = "Montserrat", color="#fff")), 
+                     plotOptions = list(series = list(dataLabels = list(style = list(fontFamily = "Montserrat", 
+                                                                                     color="#FFD100",
+                                                                                     fontSize = "1.4em",
+                                                                                     textOutline = FALSE)))),
+                     yAxis = list(labels = list(style = list(fontFamily = "Montserrat", color="#fff")), 
                                   title = list(style = list(color = "#fff", fontSize = "12px", 
                                                             color="#fff",fontWeight="500")), 
                                   gridLineWidth = 0,
@@ -555,6 +567,12 @@ output$map <- renderLeaflet({
     
     output$cnty_curves_hchart <- renderHighchart({
       
+      # Make sure requirements are met
+     req(input$countyname)
+      # 
+      # req(exists(input$countyname, "package:datasets", inherits = FALSE),
+      #     )
+      
       nyt_county_cases_chart <- nyt_county_cases %>% 
         filter(county==input$countyname) %>% 
         mutate(date = ymd(date))
@@ -562,6 +580,9 @@ output$map <- renderLeaflet({
       hcoptslang <- getOption("highcharter.lang")
       hcoptslang$thousandsSep <- ","
       options(highcharter.lang = hcoptslang)
+      
+      hc_add_series(fit, type = "line", hcaes(x = carat, y = .fitted),
+                    name = "Fit", id = "fit") 
       
       nyt_county_cases_chart %>% 
       hchart("area", hcaes(x = date, y = cases), animation=FALSE,
